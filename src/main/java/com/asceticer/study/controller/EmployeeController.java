584f37e7aa4e3dc3cb66122014430fb641718f86
Asceticer <451046357@qq.com>
2020-03-19 17:25:49 +0800

    springboot 员工添加

--- src/main/java/com/asceticer/study/controller/EmployeeController.java
+++ src/main/java/com/asceticer/study/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.asceticer.study.controller;
 
+import com.asceticer.study.dao.DepartmentDao;
 import com.asceticer.study.dao.EmployeeDao;
+import com.asceticer.study.entities.Department;
 import com.asceticer.study.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -24,11 +27,27 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
     @GetMapping("/employees")
     public String listEmployees(ModelMap map) {
         Collection<Employee> employees = employeeDao.getAll();
         map.put("employees", employees);
         return "employee/list";
+    }
+
+    @GetMapping("/employee")
+    public String toAddEmployee(ModelMap map) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        map.put("departments", departments);
+        return "employee/add";
+    }
+
+    @PostMapping("/employee")
+    public String addEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/employee/employees";
     }
 
 }
